d54c9ff1b7813e6e80dcc6f3cf7157923af43373
S00173244 <nels.puthoor@mail.itsligo.ie>
2017-11-20 15:45:29 +0000

    Score Calculator Complete

--- Week9/Week9/MainWindow.xaml.cs
+++ Week9/Week9/MainWindow.xaml.cs
@@ -15,14 +15,50 @@ using System.Windows.Shapes;
 
 namespace Week9
 {
+
+   
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        int scoreTotal = 0;
+        int scoreCount = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            int score = int.Parse(txt_score.Text);
+            scoreTotal += score;
+            scoreCount++;
+
+            txt_sTotal.Text = scoreTotal.ToString();
+            txt_sCount.Text = scoreCount.ToString();
+            txt_average.Text = (scoreTotal / scoreCount).ToString();
+
+            txt_score.Focus();
+
+        }
+
+        private void btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+            scoreTotal = 0;
+            scoreCount = 0;
+            txt_score.Text = 0.ToString();
+            txt_sTotal.Text = 0.ToString();
+            txt_sCount.Text = 0.ToString();
+            txt_average.Text = 0.ToString();
+
+            txt_score.Focus();
+        }
+
+        private void btn_exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
