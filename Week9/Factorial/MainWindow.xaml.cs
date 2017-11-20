a0a8abc82999c661feec194df7b68c8d6bc5f88e
S00173244 <nels.puthoor@mail.itsligo.ie>
2017-11-20 16:04:42 +0000

    Factorial Almost complete

---
+++ Week9/Factorial/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Factorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calculate_Click(object sender, RoutedEventArgs e)
+        {
+            decimal factorial = 1;
+            for (int i = 1; i <= int.Parse(txt_number.Text); i++)
+            {
+                factorial *= i;
+            }
+
+            
+            txt_factorial.Text = String.Format("{0: ###,###}", factorial.ToString());
+            
+
+        }
+    }
+}
